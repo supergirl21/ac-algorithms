10457644026051b4e83ea01712025540031522ac
supergirl <supergirl@anniecannons.com>
2020-11-05 10:11:44 -0800

    added the function for isBigger activity

--- isBigger/isBigger.js
+++ isBigger/isBigger.js
@@ -2,6 +2,14 @@
 Given two numbers, a, b, as inputs, return true if a > b,
 return false if a < b, return 0 otherwise.
 
+Create a function that takes in 2 parameters/inputs
+Use if/else conditional statement
+	is a bigger thatn b?
+	is a smaller than b?
+	is a equal to b?
+
+Output: print/console.log "true", "false" or 0
+
 Bonus 1: If a or b is undefined or not a number, then return -1.
 Hint: Chekout typeof() operator at
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
@@ -37,9 +45,17 @@ console.log(result) // -1
 
 **/
 
-
+	var a = parseInt(prompt("Give a value for the first number."));
+	var b = parseInt(prompt("Give a value for the second number."));
 
 function isBigger(a, b) {
-	//Your code here
 	
+	if  (a > b) {
+		console.log("True");
+	} else if  (a < b) {
+		console.log("False");
+	} else
+		console.log("0");
 }
+isBigger(a,b);
+
